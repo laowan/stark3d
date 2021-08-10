0ff8796acba7ef27473005f36ff2ccaa2a7c1516
wanzhiwen <wanzhiwen@yy.com>
2021-08-11 00:13:31 +0800

    test vulkan

--- app/main.cpp
+++ app/main.cpp
@@ -1,32 +1,36 @@
-﻿//========================================================================
-// Simple multi-window test
-// Copyright (c) Camilla Löwy <elmindreda@glfw.org>
-//
-// This software is provided 'as-is', without any express or implied
-// warranty. In no event will the authors be held liable for any damages
-// arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-//
-// 1. The origin of this software must not be misrepresented; you must not
-//    claim that you wrote the original software. If you use this software
-//    in a product, an acknowledgment in the product documentation would
-//    be appreciated but is not required.
-//
-// 2. Altered source versions must be plainly marked as such, and must not
-//    be misrepresented as being the original software.
-//
-// 3. This notice may not be removed or altered from any source
-//    distribution.
-//
-//========================================================================
-//
-// This test creates four window and clears each in a different color
-//
-//========================================================================
+﻿#define USE_VULKAN
 
+#ifdef USE_VULKAN
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
+
+#else
 #include "GL/glew.h"
 #include "GL/wglew.h"
 #include <GLFW/glfw3.h>
@@ -212,3 +216,4 @@ int main(int argc, char** argv)
     exit(EXIT_SUCCESS);
 }
 
+#endif
